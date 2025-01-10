e3aeb2bdb80bf7e959e6ab3fb76f4bb1baac8781
GillenReidSynanetics <gillen.reid@synanetics.com>
2025-01-10 15:49:22 +0000

    Menu-Dev

---
+++ menu-interface.ps1
@@ -0,0 +1,30 @@
+$mainmenu = {
+Write-Host  "***************"
+Write-Host  "* Menu *"
+Write-Host  
+Write-Host  "1. Backup Script"
+Write-Host  "2. Validation Script"
+Write-Host  "3. Exit"
+Write-Host  "Select option and press enter"
+}
+
+Do {
+Invoke-Command $mainmenu
+$select = Read-Host
+switch ($select) 
+    {
+    1{
+
+    }  
+
+
+    2 {
+
+    }
+    
+    3{ 
+
+
+    }
+}
+While ($select -ne 5)
