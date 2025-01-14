342cbbf0a6c451bf40975466d603e4246ebae2dd
GillenReidSynanetics <gillen.reid@synanetics.com>
2025-01-14 14:18:53 +0000

    modify

--- development-notes.ps1
+++ development-notes.ps1
@@ -16,3 +16,38 @@ foreach ($store in $fhirestoreheartbeat) {
         Write-Error "store $store is down : $_"
     }
 }
+
+#==================
+
+$mainmenu = {
+    Write-Host  "***************"
+    Write-Host  "* Menu *"
+    Write-Host  
+    Write-Host  "1. Backup Script"
+    Write-Host  "2. Validation Script"
+    Write-Host  "3. Exit"
+    Write-Host  "Select option and press enter"
+    }
+    
+    Do {
+    Invoke-Command $mainmenu
+    $select = Read-Host
+    switch ($select) 
+        {
+        1{
+    
+        }  
+    
+    
+        2 {
+    
+        }
+        
+        3{ 
+    
+    
+        }
+    }
+    While ($select -ne 5)
+
+    ### =================
