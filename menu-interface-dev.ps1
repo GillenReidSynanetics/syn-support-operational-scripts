031b609b51ded02be171eeb837a43b8ebefeffae
GillenReidSynanetics <gillen.reid@synanetics.com>
2025-01-14 12:01:00 +0000

    adding

--- menu-interface-dev.ps1
+++ menu-interface-dev.ps1
@@ -1,30 +1,30 @@
 $mainmenu = {
-Write-Host  "***************"
-Write-Host  "* Menu *"
-Write-Host  
-Write-Host  "1. Backup Script"
-Write-Host  "2. Validation Script"
-Write-Host  "3. Exit"
-Write-Host  "Select option and press enter"
-}
-
-Do {
-Invoke-Command $mainmenu
-$select = Read-Host
-switch ($select) 
-    {
-    1{
-
-    }  
-
-
-    2 {
-
+    Write-Host  "***************"
+    Write-Host  "* Menu *"
+    Write-Host  
+    Write-Host  "1. Backup Script"
+    Write-Host  "2. Validation Script"
+    Write-Host  "3. Exit"
+    Write-Host  "Select option and press enter"
     }
     
-    3{ 
-
-
+    Do {
+    Invoke-Command $mainmenu
+    $select = Read-Host
+    switch ($select) 
+        {
+        1{
+    
+        }  
+    
+    
+        2 {
+    
+        }
+        
+        3{ 
+    
+    
+        }
     }
-}
-While ($select -ne 5)
+    While ($select -ne 5)
