0ff8feb73565eb5db180d643686256389296fbea
GillenReidSynanetics <gillen.reid@synanetics.com>
2025-01-22 15:27:10 +0000

    documentation

--- Beta/inventory-dev.ps1
+++ Beta/inventory-dev.ps1
@@ -1,4 +1,39 @@
-# The idea here is a local script to get a read out of the existing docker containers, images, networks and volumes.
+<#
+.SYNOPSIS
+    Script to gather Docker inventory information and save it to a CSV file.
+
+.DESCRIPTION
+    This script checks if Docker is installed and then gathers information about Docker containers, images, networks, and volumes.
+    The collected data is saved to a CSV file specified by the $outputFile variable.
+
+.PARAMETER $outputFile
+    The path to the CSV file where the Docker inventory data will be saved.
+
+.FUNCTION Test-Docker
+    Checks if Docker is installed on the system. If Docker is not installed, it writes an error message and exits the script.
+
+.FUNCTION get-docker-containers
+    Retrieves information about running Docker containers and appends it to the CSV file.
+
+.FUNCTION get-docker-images
+    Retrieves information about Docker images and appends it to the CSV file.
+
+.FUNCTION get-docker-networks
+    Retrieves information about Docker networks and appends it to the CSV file.
+
+.FUNCTION get-docker-volumes
+    Retrieves information about Docker volumes and appends it to the CSV file.
+
+.NOTES
+    Author: Gillen Reid
+    Date: 2023-10-05
+    Version: 1.0
+
+.EXAMPLE
+    .\inventory-dev.ps1
+    This will run the script and save the Docker inventory information to the specified CSV file.
+#>
+
 
 $outputFile = ".\docker_data.csv"
 
@@ -12,29 +47,24 @@ function Test-Docker {
 }
 
 function get-docker-containers {
-    docker ps --format "ID={{.ID}},Name={{.Names}},Image={{.Image}},Status={{.Status}},Ports={{.Ports}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
+    docker ps --format "ID={{.ID}},Name={{.Names}},Image={{.Image}},Status={{.Status}},Ports={{.Ports}}" | ConvertFrom-Csv | Export-Csv -Path $outputFile -Append -NoTypeInformation
 }
 
 
 function get-docker-images {
-    docker images --format "ID={{.ID}},Repository={{.Repository}},Tag={{.Tag}},Size={{.Size}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
+    docker images --format "ID={{.ID}},Repository={{.Repository}},Tag={{.Tag}},Size={{.Size}}" | ConvertFrom-Csv | Export-Csv -Path $outputFile -Append -NoTypeInformation
     
 }
 
 function get-docker-networks {
-    docker network ls --format "ID={{.ID}},Name={{.Name}},Driver={{.Driver}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
+    docker network ls --format "ID={{.ID}},Name={{.Name}},Driver={{.Driver}}" | ConvertFrom-Csv | Export-Csv -Path $outputFile -Append -NoTypeInformation
     
 }
 
 
 function get-docker-volumes {
-    docker volume ls --format "Name={{.Name}},Driver={{.Driver}},Scope={{.Scope}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
+    docker volume ls --format "Name={{.Name}},Driver={{.Driver}},Scope={{.Scope}}" | ConvertFrom-Csv | Export-Csv -Path $outputFile -Append -NoTypeInformation
 
 }
 
-$outputDirectory = Join-Path -Path $PWD -ChildPath "DockerInventory"
-if (-not (Test-Path -Path $outputDirectory)) {
-    New-Item -ItemType Directory -Path $outputDirectory | Out-Null
-}
-
-Write-Host "Inventory saved to $outputDirectory"
+Write-Host "Inventory saved to $outputFile"
--- Beta/reboot-docker.ps1
+++ Beta/reboot-docker.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+    Script to reboot a Docker Compose project by stopping and removing containers, including orphan containers, and then starting the project again.
+
+.DESCRIPTION
+    This script prompts the user to enter the path to a Docker Compose project directory, validates the input, and navigates to the specified directory.
+    It then confirms with the user before running the 'docker-compose down --remove-orphans' command to stop and remove the project, including orphan containers.
+    If the user confirms, the script executes the Docker Compose down command with the --remove-orphans flag, followed by the Docker Compose up command to start the project again.
+    Finally, the script returns to the original location.
+
+.PARAMETER ProjectDir
+    The path to the Docker Compose project directory entered by the user.
+
+.NOTES
+    - The script requires Docker Compose to be installed and available in the system's PATH.
+    - The script should be run with appropriate permissions to manage Docker containers.
+
+.EXAMPLE
+    PS> .\reboot-docker.ps1
+    Enter the path to the Docker Compose project directory: C:\path\to\project
+    Are you sure you want to stop and remove the project, including orphan containers? (yes/no): yes
+    Docker Compose project has been stopped, and orphan containers have been removed.
+#>
 $scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 
 # Prompt the user to enter the Docker Compose project name or directory
