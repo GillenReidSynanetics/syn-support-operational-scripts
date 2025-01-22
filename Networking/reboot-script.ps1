f9f6912de80609462df1ec70199ac60d62f15dbc
GillenReidSynanetics <gillen.reid@synanetics.com>
2025-01-22 15:14:46 +0000

    Documenting

--- Networking/reboot-script.ps1
+++ Networking/reboot-script.ps1
@@ -1,3 +1,33 @@
+<#
+.SYNOPSIS
+    Script to automatically restart a specified service and log the actions.
+
+.DESCRIPTION
+    This script stops and starts a specified service, logging each action to a log file. 
+    It is designed to be used with scheduled tasks but can be adapted for any service.
+
+.PARAMETER logFilePath
+    The path to the log file where messages will be recorded.
+
+.PARAMETER message
+    The message to be logged.
+
+.FUNCTIONS
+    Log-Message
+        Logs a message with a timestamp to the specified log file.
+
+.NOTES
+    File Name  : reboot-script.ps1
+    Author     : GillenReidSynanetics
+    Version    : 1.0
+    Date       : YYYY-MM-DD
+    Requires   : PowerShell 5.0 or later
+
+.EXAMPLE
+    .\reboot-script.ps1
+    This will execute the script, stopping and starting the specified service and logging the actions.
+
+#>
 # Script was originally designed for automatically restarting a service via scheduled tasks - can be adapted for any service
 
 
