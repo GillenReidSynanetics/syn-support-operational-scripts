2f82566bd91ef9d00349af4243d8eb89f8ea841f
GillenReidSynanetics <gillen.reid@synanetics.com>
2025-01-22 15:26:29 +0000

    test

--- Beta/inventory-dev.ps1
+++ Beta/inventory-dev.ps1
@@ -1,5 +1,8 @@
 # The idea here is a local script to get a read out of the existing docker containers, images, networks and volumes.
 
+$outputFile = ".\docker_data.csv"
+
+@("Type,Name,Details") | Set-Content -Path $outputFile
 
 function Test-Docker {
     if (-not(Get-Command "docker" -ErrorAction SilentlyContinue)) {
@@ -9,23 +12,23 @@ function Test-Docker {
 }
 
 function get-docker-containers {
-    docker ps --format "ID={{.ID}},Name={{.Names}},Image={{.Image}},Status={{.Status}},Ports={{.Ports}}" | ConvertFrom-Csv
+    docker ps --format "ID={{.ID}},Name={{.Names}},Image={{.Image}},Status={{.Status}},Ports={{.Ports}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
 }
 
 
 function get-docker-images {
-    docker images --format "ID={{.ID}},Repository={{.Repository}},Tag={{.Tag}},Size={{.Size}}" | ConvertFrom-Csv
+    docker images --format "ID={{.ID}},Repository={{.Repository}},Tag={{.Tag}},Size={{.Size}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
     
 }
 
 function get-docker-networks {
-    docker network ls --format "ID={{.ID}},Name={{.Name}},Driver={{.Driver}}" | ConvertFrom-Csv
+    docker network ls --format "ID={{.ID}},Name={{.Name}},Driver={{.Driver}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
     
 }
 
 
 function get-docker-volumes {
-    docker volume ls --format "Name={{.Name}},Driver={{.Driver}},Scope={{.Scope}}" | ConvertFrom-Csv
+    docker volume ls --format "Name={{.Name}},Driver={{.Driver}},Scope={{.Scope}}" | ConvertFrom-Csv Export-Csv -Path $outputFile -Append -NoTypeInformation
 
 }
 
@@ -33,10 +36,5 @@ $outputDirectory = Join-Path -Path $PWD -ChildPath "DockerInventory"
 if (-not (Test-Path -Path $outputDirectory)) {
     New-Item -ItemType Directory -Path $outputDirectory | Out-Null
 }
-
-get-docker-volumes | Export-Csv -Path (Join-Path -Path $outputDirectory -ChildPath "volumes.csv") -NoTypeInformation
-get-docker-networks |  Export-Csv -Path (Join-Path -Path $outputDirectory -ChildPath "networks.csv") -NoTypeInformation
-get-docker-images | Export-Csv -Path (Join-Path -Path $outputDirectory -ChildPath "images.csv") -NoTypeInformation
-get-docker-containers | Export-Csv -Path (Join-Path -Path $outputDirectory -ChildPath "containers.csv") -NoTypeInformation
 
 Write-Host "Inventory saved to $outputDirectory"
