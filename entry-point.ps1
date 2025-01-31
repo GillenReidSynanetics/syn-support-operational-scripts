d1f424716f036a64f4411699ca61f519987d5563
Gillen <gillen.reid@synanetics.com>
2025-01-31 14:55:51 +0000

    Adding cat

--- entry-point.ps1
+++ entry-point.ps1
@@ -1,14 +1,14 @@
 
-param (
-    [string]$entryPoint = "entry-point.ps1"
-)
+# param (
+#     [string]$entryPoint = "entry-point.ps1"
+# )
 
-$entryPoint = "/operational-scripts/entry-point.ps1"
+# $entryPoint = "/operational-scripts/entry-point.ps1"
 
-if (Test-Path $entryPoint) {
-    Write-Host "Running Deployment"
-    . $entryPoint
-} else {
-    Write-Host "Start Up Script Not Found"
-}
+# if (Test-Path $entryPoint) {
+#     Write-Host "Running Deployment"
+#     . $entryPoint
+# } else {
+#     Write-Host "Start Up Script Not Found"
+# }
 
